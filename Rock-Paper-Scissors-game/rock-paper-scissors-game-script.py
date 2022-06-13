7ae009053c5bee01698035eb2172fe0b56cd55fb
blackdreamer15 <99067120+blackdreamer15@users.noreply.github.com>
2022-06-13 14:34:18 +0000

    Add instructions and options to see them or not

--- Rock-Paper-Scissors-game/rock-paper-scissors-game-script.py
+++ Rock-Paper-Scissors-game/rock-paper-scissors-game-script.py
@@ -1,3 +1,14 @@
 import random
 
+print("**********************************************")
+print("*    ROCK PAPER SCISSOR GAME USING PYTHON    *")
+print("**********************************************\n")
+
+# CHOOSE TO SEE INSTRUCTIONS BEFORE GAME BEGINS
+initialInput = int(input("Enter 1 to see the instructions and 2 to skip ::  "))
+if(initialInput==1):
+    print("\n\nWhen the game starts, just enter R for Rock, P for Paper and S for Scissor\nWinning Rules of the Rock paper scissor game as follows: \n"
+            +"Rock(R)  vs  Paper(P)    ->  Paper(P) wins \n"
+            +"Rock(R)  vs  Scissor(S)  ->  Rock(R) wins \n"
+            +"Paper(P) vs  Scissor(S)  ->  Scissor(S) wins \n")
 
